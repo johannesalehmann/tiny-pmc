4b603923a015963267a5d162af61c85bd8399b8d
Johannes Lehmann <johannes_alexander.lehmann@tu-dresden.de>
2026-07-14 18:51:34 +0200

    Add function to valuation class to return entry index by name

--- probabilistic-models/src/valuations/class.rs
+++ probabilistic-models/src/valuations/class.rs
@@ -42,6 +42,10 @@ impl<ClassEntryIdx: Index> ValuationClass<ClassEntryIdx> {
         &self.entries[index]
     }
 
+    pub fn index_by_name(&self, name: &str) -> Option<ClassEntryIdx> {
+        self.entries.index_by_name(name)
+    }
+
     pub fn get_by_name(&self, name: &str) -> &ValuationClassEntry {
         self.entries.entry_by_name(name).unwrap()
     }
