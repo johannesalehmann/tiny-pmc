4a8a04afd65350d079705e20be6076bbf8fe0169
Johannes Lehmann <johannes_alexander.lehmann@tu-dresden.de>
2026-07-20 17:14:24 +0200

    Add trait to read initial states

---
+++ probabilistic-models/src/traits/initial_state/mod.rs
@@ -0,0 +1,65 @@
+use crate::initial_states::SingleInitialState;
+use crate::{InitialStates, Model};
+use typed_index_collections::Index;
+
+pub trait ReadInitialStates {
+    type StateIdx: Index;
+    type InitialStatesIterator<'a>: IntoIterator<Item = Self::StateIdx>
+    where
+        Self: 'a;
+    fn is_initial(&self, state: Self::StateIdx) -> bool;
+    fn initial_states(&self) -> Self::InitialStatesIterator<'_>;
+}
+
+impl<StateIdx: Index> ReadInitialStates for SingleInitialState<StateIdx> {
+    type StateIdx = StateIdx;
+    type InitialStatesIterator<'a>
+        = std::iter::Once<StateIdx>
+    where
+        StateIdx: 'a;
+
+    fn is_initial(&self, state: Self::StateIdx) -> bool {
+        state == self.index
+    }
+
+    fn initial_states(&self) -> Self::InitialStatesIterator<'_> {
+        std::iter::once(self.index)
+    }
+}
+
+impl<StateIdx: Index> ReadInitialStates for InitialStates<StateIdx> {
+    type StateIdx = StateIdx;
+    type InitialStatesIterator<'a>
+        = typed_index_collections::To1BoolValuesIterator<
+        StateIdx,
+        &'a typed_index_collections::To1<StateIdx, bool>,
+    >
+    where
+        StateIdx: 'a;
+
+    fn is_initial(&self, state: Self::StateIdx) -> bool {
+        self[state]
+    }
+
+    fn initial_states(&self) -> Self::InitialStatesIterator<'_> {
+        self.true_values().into_iter()
+    }
+}
+
+impl<M, Ini: ReadInitialStates, ChLabel, BrLabel, Obs, APs, Rew, Ann, StateVals, Preds>
+    ReadInitialStates for Model<M, Ini, ChLabel, BrLabel, Obs, APs, Rew, Ann, StateVals, Preds>
+{
+    type StateIdx = Ini::StateIdx;
+    type InitialStatesIterator<'a>
+        = Ini::InitialStatesIterator<'a>
+    where
+        Self: 'a;
+
+    fn is_initial(&self, state: Self::StateIdx) -> bool {
+        self.initial.is_initial(state)
+    }
+
+    fn initial_states(&self) -> Self::InitialStatesIterator<'_> {
+        self.initial.initial_states()
+    }
+}
