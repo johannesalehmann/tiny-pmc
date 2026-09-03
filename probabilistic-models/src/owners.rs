efe1897d76bf405c91c158259b34dcc0e6ffbf54
Johannes Lehmann <johannes_alexander.lehmann@tu-dresden.de>
2026-09-03 15:05:30 +0200

    Add note on `SinglePlayer` ownership enum

--- probabilistic-models/src/owners.rs
+++ probabilistic-models/src/owners.rs
@@ -1,3 +1,6 @@
+// TODO: This is currently unused. It would be useful to implement ReadOwners for models that have
+//  no owners. That way, algorithms can require M: ReadOwners<Type=SinglePlayer> to ensure that no
+//  model with owners is passed in (which the algorithm might not be able to handle)
 #[derive(Copy, Clone)]
 pub enum SinglePlayer {
     SinglePlayer,
